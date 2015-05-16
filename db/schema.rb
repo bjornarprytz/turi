c14c0db3913688d07b4b1c5291444a7215ca262d
Bjornar Prytz <bpr000@post.uit.no>
2015-05-16 16:11:00 +0200

    finished model and controller tests for waypoints and routes

--- db/schema.rb
+++ db/schema.rb
@@ -154,7 +154,6 @@ ActiveRecord::Schema.define(version: 20150508012530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "trip_id"
-    t.boolean  "public",     default: false
   end
 
   add_index "routes", ["trip_id"], name: "index_routes_on_trip_id"
